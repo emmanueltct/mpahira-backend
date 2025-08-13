894408c0a9aba88c3cc13f6b07771b4fcd16ed2f
MUNEZERO Emmanuel <emmanuelmunezero@gmail.com>
2025-08-14 01:47:49 +0200

    checkout bug fixing

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -46,7 +46,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `${process.env.baseURL}api/auth/google/callback`, 
+      callbackURL: `${process.env.baseURL}/api/auth/google/callback`, 
       
     },
     async (_accessToken, _refreshToken, profile, done) => {
